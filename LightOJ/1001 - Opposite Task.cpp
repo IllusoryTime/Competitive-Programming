f06175b03c9d63559837c157ce45a8422e08a937
Afzal Hussain <afzal.hussain3210@gmail.com>
2016-09-01 15:08:48 +0600

    Create 1001 - Opposite Task.cpp

---
+++ LightOJ/1001 - Opposite Task.cpp
@@ -0,0 +1,16 @@
+//Source Code Written: 1/9/13
+
+#include <stdio.h>
+
+int main() {
+    int t, n;
+    scanf("%d",&t);
+    for(int i=1; i<=t; i++) {
+        scanf("%d", &n);
+        if(n<10)
+            printf("0 %d\n",n);
+        else printf("%d %d\n",n-10,10);
+    }
+    return 0;
+}
+
