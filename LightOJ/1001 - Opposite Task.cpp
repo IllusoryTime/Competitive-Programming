7efad71751819d2e02b08a792868dfe95bbf70e4
Afzal Hussain <afzal.hussain3210@gmail.com>
2016-09-02 18:49:13 +0600

    Update 1001 - Opposite Task.cpp

--- LightOJ/1001 - Opposite Task.cpp
+++ LightOJ/1001 - Opposite Task.cpp
@@ -4,12 +4,12 @@
 
 int main() {
     int t, n;
-    scanf("%d",&t);
+    scanf("%d", &t);
     for(int i=1; i<=t; i++) {
         scanf("%d", &n);
-        if(n<10)
-            printf("0 %d\n",n);
-        else printf("%d %d\n",n-10,10);
+        if(n < 10)
+            printf("0 %d\n", n);
+        else printf("%d %d\n", n-10, 10);
     }
     return 0;
 }
