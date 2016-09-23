3399d6d31db2d0f31e294e2cef6cfc9de9e56b20
IllusoryTime <afzal.hussain3210@gmail.com>
2016-09-23 22:09:27 +0600

    Create 1124 - Celebrity jeopardy.cpp

---
+++ UVa-Online-Judge/1124 - Celebrity jeopardy.cpp
@@ -0,0 +1,12 @@
+//Source Code Written: 5/23/15
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    while(getline(cin, str)) {
+        cout << str << endl;
+    }
+    return 0;
+}
