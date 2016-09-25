2f6519c138c33491aab65a02945c8879314765de
IllusoryTime <afzal.hussain3210@gmail.com>
2016-09-25 18:15:35 +0600

    Create 490 - Rotating Sentences.cpp

---
+++ UVa-Online-Judge/490 - Rotating Sentences.cpp
@@ -0,0 +1,23 @@
+//Source Code Written: 6/3/16
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //freopen("in.txt", "r", stdin);
+    string str[100+9];
+    int total = 0, max_len = 0, len;
+    while(getline(cin, str[total])) {
+        len = str[total].size();
+        if(len > max_len) max_len = len;
+        total++;
+    }
+    for(int i=0; i<max_len; i++) {
+        for(int j=total-1; j>=0; j--) {
+            if(i < str[j].size()) printf("%c", str[j][i]);
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
